ac0af3e217ec73224592cf2d1e4d856bc4c98f44
AlexSacMorane <90468713+AlexSacMorane@users.noreply.github.com>
2023-01-18 16:16:28 +0100

    change parameter

--- Sample_case/User.py
+++ Sample_case/User.py
@@ -38,7 +38,6 @@ def All_parameters():
     grain_discretisation = 40 # = grain_discretisation_square
 
     N_grain = 300 #total number of grains
-    frac_dissolved = 0 #V_soluble/V_total
 
     #Disk
     R_mean = 350 #µm radius to compute the grain distribution. Then recomputed
@@ -48,31 +47,18 @@ def All_parameters():
     R_mean = 0
     for i in range(len(L_R)):
         R_mean = R_mean + L_R[i]*L_percentage_R[i]
-    #Square
-    Dimension_mean = 700 #µm radius
-    L_Dimension = [1.2*Dimension_mean,1.1*Dimension_mean,0.9*Dimension_mean,0.8*Dimension_mean] #from larger to smaller
-    L_percentage_Dimension = [1/6,1/3,1/3,1/6] #distribution of the different radius
-    #Recompute the mean dimension
-    Dimension_mean = 0
-    for i in range(len(L_Dimension)):
-        Dimension_mean = Dimension_mean + L_Dimension[i]*L_percentage_Dimension[i]
 
     #Compute number of grain (square or disk)
-    N_grain_square = int(N_grain*frac_dissolved*math.pi*R_mean**2/(frac_dissolved*math.pi*R_mean**2 + Dimension_mean*Dimension_mean*(1-frac_dissolved)))
-    N_grain_disk = N_grain - N_grain_square
+    N_grain_disk = N_grain
 
     #write dict
     dict_geometry = {
+    'N_grain' : N_grain,
     'N_grain_disk' : N_grain_disk,
     'R_mean' : R_mean,
     'L_R' : L_R,
     'L_percentage_R' : L_percentage_R,
-    'grain_discretisation' : grain_discretisation,
-    'N_grain_square' : N_grain_square,
-    'Dimension_mean' : Dimension_mean,
-    'L_Dimension' : L_Dimension,
-    'L_percentage_Dimension' : L_percentage_Dimension,
-    'grain_discretisation_square' : grain_discretisation,
+    'grain_discretisation' : grain_discretisation
     }
 
     #---------------------------------------------------------------------------
@@ -82,7 +68,6 @@ def All_parameters():
     nu = 0.3 #Poisson's ratio
     rho = 2500*10**(-6*3) #density kg/µm3
     rho_surf_disk = 4/3*rho*R_mean #kg/µm2
-    rho_surf_square = rho*Dimension_mean #kg/µm2
     mu_friction_gg = 0.5 #grain-grain
     mu_friction_gw = 0 #grain-wall
     coeff_restitution = 0.2 #1 is perfect elastic
@@ -95,8 +80,7 @@ def All_parameters():
     'Y' : Y,
     'nu' : nu,
     'rho' : rho,
-    'rho_surf_disk' : rho_surf_disk,
-    'rho_surf_square' : rho_surf_square,
+    'rho_surf' : rho_surf_disk,
     'mu_friction_gg' : mu_friction_gg,
     'mu_friction_gw' : mu_friction_gw,
     'coeff_restitution' : coeff_restitution,
@@ -107,7 +91,7 @@ def All_parameters():
     #---------------------------------------------------------------------------
     #Sample definition
 
-    Lenght_mean = (R_mean*N_grain_disk + Dimension_mean/2*N_grain_square)/N_grain #mean characteristic lenght
+    Lenght_mean = R_mean*N_grain_disk /N_grain #mean characteristic lenght
 
     #Box définition
     x_box_min = 0 #µm
@@ -130,8 +114,8 @@ def All_parameters():
     factor_distribution_etai = 1.5 #margin to distribute etai
     MovePF_selector = 'DeconstructRebuild' #Move PF
     n_local = 40 #number of node inside local PF simulation
-    dx_local = min(2*min(dict_geometry['L_R']),min(dict_geometry['L_Dimension']))/(n_local-1)
-    dy_local = min(2*min(dict_geometry['L_R']),min(dict_geometry['L_Dimension']))/(n_local-1)
+    dx_local = 2*min(dict_geometry['L_R'])/(n_local-1)
+    dy_local = 2*min(dict_geometry['L_R'])/(n_local-1)
     #add into material dict from this data
     w = 4*math.sqrt(dx_local**2+dy_local**2)
     double_well_height = 10*dict_material['kc_pf']/w/w
@@ -139,7 +123,7 @@ def All_parameters():
     dict_material['double_well_height'] = double_well_height
 
     #DEM parameters
-    dt_DEM_crit = math.pi*min(min(L_Dimension)/2,min(L_R))/(0.16*nu+0.88)*math.sqrt(rho*(2+2*nu)/Y) #s critical time step from O'Sullivan 2011
+    dt_DEM_crit = math.pi*min(L_R)/(0.16*nu+0.88)*math.sqrt(rho*(2+2*nu)/Y) #s critical time step from O'Sullivan 2011
 
     dt_DEM = dt_DEM_crit/8 #s time step during DEM simulation
     factor_neighborhood = 1.5 #margin to detect a grain into a neighborhood
@@ -201,7 +185,6 @@ def All_parameters():
     #Initial condition parameters
 
     n_generation = 2 #number of grains generation
-    #/!\ Work only for 2 /!\
     factor_ymax_box = 1.8 #margin to generate grains
     N_test_max = 5000 # maximum number of tries to generate a grain without overlap
     i_DEM_stop_IC = 4000 #stop criteria for DEM during IC
